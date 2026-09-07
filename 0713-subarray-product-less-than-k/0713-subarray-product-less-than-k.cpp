74b69b53d6df087f236bb6ad56d5797ea2ab175f
Shreyas Jha <196503376+Zaa-Shreyas@users.noreply.github.com>
2026-09-07 11:33:22 +0530

    0713-SUBARRAY-PRODUCT-LESS-THAN-K Stats: Time: 0 ms (100%), Space: 44.9 MB (5.79%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -4,6 +4,7 @@ public:
         int left = 0;
         int count = 0;
         int prod = 1;
+        if (k <= 1) return 0;
         for(int right = 0; right <nums.size(); right++){
             prod *= nums[right];
             while(prod >= k && left<=right){
